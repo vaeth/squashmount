870aba7d4ac880f4d599007b969f770091f3e4be
Martin Väth <vaeth@mathematik.uni-wuerzburg.de>
2013-08-18 17:49:23 +0200

    Improve documentation. Fix rounding. Fix sanity checks. Try to skip remount

--- etc/squashmount.pl
+++ etc/squashmount.pl
@@ -1,48 +1,70 @@
 #!/usr/bin/perl (this is only for editors)
 
-# We use local variables to make the paths easily movable.
-
-my $root = '';
-my $portage = "$root/usr/portage";
-my $tex = "$root/usr/share/texmf-dist";
-my $db = "$root/var/db"
+# As some first examples, we configure mount points "manually":
 
 push(@mounts, {
-	TAG => 'portage',
-	DIR => $portage,
-	CHANGES => "$portage.changes",
-	READONLY => "$portage.readonly",
-	FILE => "$portage.sqfs",
-	TEMPDIR => "$root/tmp",
-	THRESHOLD => "40m"
+	TAG => 'guest-user',
+	DIR => '/home/guest',
+	FILE => '/home/guest-skeleton.sqfs',
+	KILL => 1 # normally remove data on every umount/remount
+	# If you want to cancel this KILL temporarily
+	# (e.g. to make modifications on guest-skeleton.sqsf)
+	# use something like "squashmount --nokill set"
 }, {
-	TAG => 'tex',
-	DIR => $tex,
-	CHANGES => "$tex.changes",
-	READONLY => "$tex.readonly",
-	FILE => "$tex.sqfs",
-	TEMPDIR => "$root/tmp",
-
-	# This huge directory takes ages to compress with xz:
-	COMPRESSION => 'lzo',
-
-	# Ignore regeneration (with different timestampt of the files
-	#     ls-R or tex/generic/config/language.*
-	# Note that we also have to ignore changes in the parent directories:
-	SKIP => [ '^tex$', '^tex/generic$', '^tex/generic/config$' ],
-	DIFF => [ '^ls-R$', '^tex/generic/config/language\.(dat(|\.lua)|def)$' ]
+	TAG => 'fixed',
+	DIR => '/fixed/dir',
+	FILE => '/fixed/content.sqfs',
+	READONLY => 1 # Do not use overlayfs/aufs/...: Non writable directory
 }, {
 	TAG => 'db',
-	DIR => $db,
-	CHANGES => "$db.changes",
-	READONLY => "$db.readonly",
-	FILE => "$db.sqfs",
+	DIR = '/var/db',
+	FILE = '/var/db.sqfs',
+	BACKUP = '/var/db.sqfs.bak', # keep a backup
+	CHANGES = '/var/db.changes',
+	READONLY = '/var/db.readonly',
 
-	# Keep a bakup
-	BACKUP => "$db.sqfs.bak",
-	TEMPDIR => "$root/tmp"
-
-	# A huge blocksize makes number of files more important than their size
-	BLOCKSIZE => 65536,
-	THRESHOLD => "2m"
+	# Do not resquash everytime but only after 30 megabytes of fresh data:
+	THRESHOLD = '30m',
+	# Since this directory contains only very small files, we cheat with
+	# this size by using that each file takes at least a full block:
+	# Hence, the number of files is more important for THRESHOLD than
+	# their size. In gentoo, one installed package thus "counts" about
+	# 2m in size (although it is actually only 20 very short files):
+	BLOCKSIZE = 65536
 });
+
+# Other examples are generated with the provided standard_hash function:
+# Recall that this function fills TAG, DIR, FILE, CHANGES, READONLY, and
+# optionally also a BACKUP.
+
+# For example, after the following commands, %db is the same hash as above:
+my $root = '';
+my %db = &standard_hash($root, '/var/db', 'db', 1);
+$db{'THRESHOLD'} = $db{'THRESHOLD'} = '30m';
+$db{'BLOCKSIZE'} = $db{'BLOCKSIZE'} = 65536; # about 2m per package
+
+# We configure tex as in the "squashmount man" example:
+my %tex= &standard_hash($root, '/usr/share/texmf-dist', 'tex');
+$tex{'SKIP'} = $tex{'SKIP'} = '^ls-R$';
+$tex{'DIFF'} = $tex{'DIFF'} = '^tex(/generic(/config(/language(\.(dat(\.lua)?|def)))?)?)?$';
+
+my %portage = &standard_hash($root, '/usr/portage', 'portage');
+$portage{'THRESHOLD'} = '40m';
+
+my %kernel = &standard_hash($root, '/usr/src', 'kernel');
+my %office = &standard_hash($root, '/usr/lib/libreoffice', 'office');
+my %games = &standard_hash($root, '/usr/share/games', 'games');
+$games{'COMPRESSION'} = 'lzo'; # games is a huge directory: compress it faster
+
+# After we have defined all hashes, we tell squashmount to use them.
+# We need of course not necessarily use the order in which the variables
+# were defined:
+
+push(@mounts,
+# db is not pushed since this was configured earlier
+	\%games,
+	\%office,
+	\%portage,
+	\%tex,
+	\%kernel
+);
