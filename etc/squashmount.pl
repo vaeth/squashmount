2cb3ced4fac9c17ce4e18eb951d916966079db2b
Martin Väth <vaeth@mathematik.uni-wuerzburg.de>
2013-08-19 20:28:20 +0200

    reset, -r, forget, stop, restart, creat, &standard_mount, remove &standard_hash

--- etc/squashmount.pl
+++ etc/squashmount.pl
@@ -1,70 +1,77 @@
 #!/usr/bin/perl (this is only for editors)
 
-# As some first examples, we configure mount points "manually":
+# The tools which we have installed; if possible only the first in this list
+# is used, but the others are a fallback if that fails.
 
-push(@mounts, {
-	TAG => 'guest-user',
-	DIR => '/home/guest',
-	FILE => '/home/guest-skeleton.sqfs',
-	KILL => 1 # normally remove data on every umount/remount
-	# If you want to cancel this KILL temporarily
-	# (e.g. to make modifications on guest-skeleton.sqsf)
-	# use something like "squashmount --nokill set"
-}, {
-	TAG => 'fixed',
-	DIR => '/fixed/dir',
-	FILE => '/fixed/content.sqfs',
-	READONLY => 1 # Do not use overlayfs/aufs/...: Non writable directory
-}, {
-	TAG => 'db',
-	DIR = '/var/db',
-	FILE = '/var/db.sqfs',
-	BACKUP = '/var/db.sqfs.bak', # keep a backup
-	CHANGES = '/var/db.changes',
-	READONLY = '/var/db.readonly',
+@order = ('overlayfs', 'aufs', 'unionfs-fuse', 'unionfs', 'funionfs');
 
-	# Do not resquash everytime but only after 30 megabytes of fresh data:
-	THRESHOLD = '30m',
-	# Since this directory contains only very small files, we cheat with
-	# this size by using that each file takes at least a full block:
-	# Hence, the number of files is more important for THRESHOLD than
-	# their size. In gentoo, one installed package thus "counts" about
-	# 2m in size (although it is actually only 20 very short files):
-	BLOCKSIZE = 65536
-});
+# Even if we define following is empty it is convenient to use
+# this local variable throughout, so that we can simply change it:
 
-# Other examples are generated with the provided standard_hash function:
-# Recall that this function fills TAG, DIR, FILE, CHANGES, READONLY, and
-# optionally also a BACKUP.
-
-# For example, after the following commands, %db is the same hash as above:
-my $root = '';
-my %db = &standard_hash($root, '/var/db', 'db', 1);
-$db{'THRESHOLD'} = $db{'THRESHOLD'} = '30m';
-$db{'BLOCKSIZE'} = $db{'BLOCKSIZE'} = 65536; # about 2m per package
-
-# We configure tex as in the "squashmount man" example:
-my %tex= &standard_hash($root, '/usr/share/texmf-dist', 'tex');
-$tex{'SKIP'} = $tex{'SKIP'} = '^ls-R$';
-$tex{'DIFF'} = $tex{'DIFF'} = '^tex(/generic(/config(/language(\.(dat(\.lua)?|def)))?)?)?$';
-
-my %portage = &standard_hash($root, '/usr/portage', 'portage');
-$portage{'THRESHOLD'} = '40m';
-
-my %kernel = &standard_hash($root, '/usr/src', 'kernel');
-my %office = &standard_hash($root, '/usr/lib/libreoffice', 'office');
-my %games = &standard_hash($root, '/usr/share/games', 'games');
-$games{'COMPRESSION'} = 'lzo'; # games is a huge directory: compress it faster
-
-# After we have defined all hashes, we tell squashmount to use them.
-# We need of course not necessarily use the order in which the variables
-# were defined:
+my $defaults = {
+	COMPRESSION => 'xz'
+};
 
 push(@mounts,
-# db is not pushed since this was configured earlier
-	\%games,
-	\%office,
-	\%portage,
-	\%tex,
-	\%kernel
+	&added_hash($defaults, {
+		TAG => 'guest',
+		DIR => '/home/guest',
+		FILE => '/home/guest-skeleton.sqfs',
+		KILL => 1 # normally remove data on every umount/remount
+		# If you want to cancel this KILL temporarily
+		# (e.g. to make modifications on guest-skeleton.sqsf)
+		# use something like "squashmount --nokill set"
+	}), &added_hash($defaults, {
+		TAG => 'fixed',
+		DIR => '/fixed/dir',
+		FILE => '/fixed/content.sqfs',
+		READONLY => 1 # Do not use overlayfs/aufs/...
+	}), &added_hash($defaults, {
+		TAG => 'db',
+		DIR => '/var/db',
+		FILE => '/var/db.sqfs',
+		BACKUP => 1, # keep a backup in /var/db.sqfs.bak
+		             # For a different path, we could have written:
+		             # BACKUP => '/backup-disk/db.sqfs'
+		CHANGES => '/var/db.changes',
+		READONLY => '/var/db.readonly',
+
+		# If /var is on a separate partition, we want that the
+		# squashfile is first generated in /var/tmp:
+		#TEMPDIR => '/var/tmp',
+
+		# Do not resquash everytime but only after
+		# 30 megabytes of fresh data:
+		THRESHOLD => '30m',
+		# Since this directory contains only very small files,
+		# we cheat with this size by using that each file takes
+		# at least a full block:
+		# Hence, the number of files is more important for THRESHOLD
+		# than their size. In gentoo, one installed package thus
+		# "counts" about 2m in size
+		# (although it is actually only 20 very short files):
+		BLOCKSIZE => 65536
+	}),
+# Instead of specifying TAG, DIR, FILE, CHANGES explicitly,
+# we use now that they are specified analogously to the above example
+# with the standard_mount function.
+	&standard_mount('kernel', '/usr/src', $defaults),
+# The above is actually equivalent to
+#	{ TAG => 'kernel', DIR => '/usr/src', FILE => '/usr/src.sqfs',
+#	CHANGES => '/usr/src.changes', READONLY => '/usr/src.readonly' },
+
+# We configure tex as in the "squashmount man" example:
+	&standard_mount('text', '/usr/share/texmf-dist', $defaults, {
+		SKIP => '^ls-R$',
+		DIFF => '^tex(/generic(/config(/language(\.(dat(\.lua)?|def)))?)?)?$'
+	}),
+	&standard_mount('portage', '/usr/portage', $defaults, {
+		THRESHOLD => '80m'
+	}),
+	&standard_mount('games', '/usr/share/games', $defaults, {
+		# games is huge: use the fastest compression algorithm for it.
+		# (Note that this overrides $defaults):
+		COMPRESSION => 'lzo'
+	}),
+	&standard_mount('office', '/usr/lib/libreoffice', $defaults)
 );
